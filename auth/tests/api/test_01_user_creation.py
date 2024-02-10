33cf171156e02ca6650611f1bcaeba1dd4fde13b
AngryWolf <qrer71@yandex.ru>
2024-02-10 13:26:39 +0300

    Add a user re-registration verification test

--- auth/tests/api/test_01_user_creation.py
+++ auth/tests/api/test_01_user_creation.py
@@ -1,7 +1,7 @@
 import pytest
 from httpx import AsyncClient
 
-LENGTH_USERNAME = 255
+from .constants import INVALID_LENGTH
 
 
 async def test_01_register_user(async_client: AsyncClient) -> None:
@@ -10,7 +10,6 @@ async def test_01_register_user(async_client: AsyncClient) -> None:
                                        json={'username': 'UserTestAdmin',
                                              'email': 'usertest@test.com',
                                              'password': 'testPassword12_'})
-    print(response.json())
     assert response.status_code == 201, (
         'При регистрации пользователя должен возвращаться статус-код 201.'
     )
@@ -45,8 +44,11 @@ async def test_01_register_user(async_client: AsyncClient) -> None:
      'email': 'u' * 255 + '@test.com',
      'password': 'testPassword12_'}
 ])
-async def test_02_register_invalid_email(async_client: AsyncClient,
-                                         json: dict[str, str]) -> None:
+async def test_02_register_invalid_email(
+        async_client: AsyncClient,
+        json: dict[str, str],
+        message_email: tuple[str, ...]
+) -> None:
     """Проверка регистрации пользователя при не валидной почты."""
     response = await async_client.post('/users/jwt/register/', json=json)
     assert response.status_code == 422, (
@@ -62,19 +64,10 @@ async def test_02_register_invalid_email(async_client: AsyncClient,
         'При некорректной регистрации пользователя '
         'в ответе должно быть значение списком.'
     )
-    error_message = (
-        ('value is not a valid email address: '
-         'The email address is not valid. '
-         'It must have exactly one @-sign.'),
-        ('value is not a valid email address: '
-         'The part after the @-sign is not valid. '
-         'It should have a period.'),
-        ('value is not a valid email address: '
-         'There must be something before the @-sign.'),
-        ('Value should have at least 6 items after validation, not 5'),
+    error_message = message_email + (
         ('value is not a valid email address: '
          'The email address is too long before the @-sign '
-         '(191 characters too many).')
+         '(191 characters too many).'),
     )
     for value in data['detail']:
         assert value['msg'] in error_message, (
@@ -137,7 +130,7 @@ async def test_03_invalid_password(async_client: AsyncClient,
     {'username': 'user',
      'email': 'usertest@test.com',
      'password': 'testPassword12_'},
-    {'username': 'UserTestAdmin' * LENGTH_USERNAME,
+    {'username': 'UserTestAdmin' * INVALID_LENGTH,
      'email': 'usertest@test.com',
      'password': 'testPassword12_'}
 ])
@@ -168,3 +161,44 @@ async def test_04_invalid_username(async_client: AsyncClient,
             'При некорректной регистрации пользователя '
             'в ответе должно быть указано описание ошибки.'
         )
+
+
+@pytest.mark.parametrize('json', [
+    {'username': 'UserTestAdmin',
+     'email': 'usertest2@test.com',
+     'password': 'testPassword12_'},
+    {'username': 'UserTestAdmin2',
+     'email': 'usertest@test.com',
+     'password': 'testPassword12_'},
+    {'username': 'UserTestAdmin',
+     'email': 'usertest@test.com',
+     'password': 'testPassword12_'}
+])
+async def test_05_repeat_register_user(async_client: AsyncClient,
+                                       json: dict[str, str]) -> None:
+    """Проверка повторной регистрации пользователя."""
+    response = await async_client.post('/users/jwt/register/',
+                                       json={'username': 'UserTestAdmin',
+                                             'email': 'usertest@test.com',
+                                             'password': 'testPassword12_'})
+    assert response.status_code == 400, (
+        'При некорректной регистрации пользователя '
+        'должен возвращаться статус-код 400.'
+    )
+    data = response.json()
+    assert list(data.keys()) == ['detail'], (
+        'При некорректной регистрации пользователя '
+        'в ответе должен быть ключ `detail`.'
+    )
+    assert type(data['detail']) == str, (
+        'При некорректной регистрации пользователя '
+        'в ответе должно быть значение строкой.'
+    )
+    error_message = (
+        ('Пользователь с таким юзернеймом уже существует.'),
+        ('Пользователь с такой почтой уже существует.')
+    )
+    assert data['detail'] in error_message, (
+        'При некорректной регистрации пользователя '
+        'в ответе должно быть указано описание ошибки.'
+    )
