9193818c9c181b966de1553f78808d30c3ae0f2c
AngryWolf <qrer71@yandex.ru>
2024-02-10 17:32:31 +0300

    Create the logic for the user update operation

--- auth/src/auth/application/services/users.py
+++ auth/src/auth/application/services/users.py
@@ -1,35 +1,68 @@
 from auth.application.protocols.unit_of_work import UoW
 from auth.core.password import get_password_hash
-from auth.domain.models import User
-from auth.domain.schemas.users import UserCreate, UserGet
+from auth.domain.schemas.users import UserCreate, UserGet, UserUpdate
 from .exceptions import (EmptyUserException,
                          InvalidEmailException,
                          InvalidUsernameException)
 
 
 class UserService:
-    async def register_user(self, uow: UoW, user: UserCreate) -> User:
+    async def register_user(self, uow: UoW, schema: UserCreate) -> UserGet:
+        """Регистрация пользователя."""
         async with uow:
-            if await self._check_username_exists(uow, user.username):
+            if await self._check_username_exists(uow, schema.username):
                 raise InvalidUsernameException(
                     'Пользователь с таким юзернеймом уже существует.'
                 )
-            elif await self._check_email_exists(uow, user.email):
+            elif await self._check_email_exists(uow, schema.email):
                 raise InvalidEmailException(
                     'Пользователь с такой почтой уже существует.'
                 )
-            user.password = get_password_hash(user.password)
-            user = await uow.users.add_one(**user.model_dump())
+            schema.password = get_password_hash(schema.password)
+            user = await uow.users.add_one(**schema.model_dump())
             await uow.commit()
-            return user
+            return user.to_read_model()
 
     async def get_user(self, uow: UoW, username: str) -> UserGet:
+        """Получение пользователя."""
         async with uow:
             if not (user := await uow.users.find_one(username=username)):
                 raise EmptyUserException()
             if user.token:
                 return user.to_read_model()
             raise EmptyUserException()
+
+    async def update_user(
+            self, uow: UoW, user_id: int, schema: UserUpdate
+    ) -> UserGet:
+        """Обновление пользователя."""
+        async with uow:
+            if await self._check_user_username(uow, user_id, schema.username):
+                raise InvalidUsernameException(
+                    'Пользователь с таким юзернеймом уже существует.'
+                )
+            elif await self._check_user_email(uow, user_id, schema.email):
+                raise InvalidEmailException(
+                    'Пользователь с такой почтой уже существует.'
+                )
+
+            if schema.password:
+                schema.password = get_password_hash(schema.password)
+            user = await uow.users.update_one(
+                user_id, **schema.model_dump(exclude_none=True)
+            )
+            await uow.commit()
+            return user.to_read_model()
+
+    async def _check_user_email(
+            self, uow: UoW, id: int, email: str
+    ) -> bool:
+        return await uow.users.check_email_user(id, email)
+
+    async def _check_user_username(
+            self, uow: UoW, id: int, username: str
+    ) -> bool:
+        return await uow.users.check_username_user(id, username)
 
     async def _check_username_exists(self, uow: UoW, username: str) -> bool:
         return await uow.users.get_user_exists(username=username)
