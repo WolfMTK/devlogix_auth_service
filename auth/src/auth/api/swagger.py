1b4e28f1e6b7500d4d50a5d29bf004b1abd4760a
AngryWolf <qrer71@yandex.ru>
2024-02-10 13:28:42 +0300

    Update the examples for swagger

--- auth/src/auth/api/swagger.py
+++ auth/src/auth/api/swagger.py
@@ -9,6 +9,7 @@ BODY_USER_CREATE_EXAMPLE = {
 
 RESPONSE_USER_CREATE_EXAMPLE = {
     201: {
+        'description': 'Пользователь успешно создан',
         'content': {
             'application/json': {
                 'example': {
@@ -20,18 +21,19 @@ RESPONSE_USER_CREATE_EXAMPLE = {
             }
         }
     },
-    404: {
+    400: {
+        'description': '',
         'content': {
             'application/json': {
                 'examples': {
-                    'Юзернейм': {
+                    'Пользователь существует (юзернейм)': {
                         'value': {
-                            'detail': 'Пользователь с таким юзернеймом уже существует!'
+                            'detail': 'Пользователь с таким юзернеймом уже существует.'
                         }
                     },
-                    'Почта': {
+                    'Пользователь существует (E-mail)': {
                         'value': {
-                            'detial': 'Пользователь с такой почтой уже существует!'
+                            'detial': 'Пользователь с такой почтой уже существует.'
                         }
                     },
                 }
@@ -132,21 +134,108 @@ RESPONSE_USER_CREATE_EXAMPLE = {
     }
 }
 
-# RESPONSE_LOGIN_EXAMPLE = {
-#     200: {
-#         'content': {
-#             'application/json': {
-#                 'example': {
-#                     'access_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJVc2VyQWRtaW4iLCJkYXRlIjoiMjAyNC0wMi0wNCAxNzozOToxMC43NjI1NzMiLCJleHAiOjE3MDcwNTc1NTB9.utFkx8oFaTEYfDStUmQV2lM7yK51IK87cjxVbqZCk0k',
-#                     'expires_in': 600,
-#                     'refresh_token': 'cc9583a2-85d7-504a-b518-b0d86506b749',
-#                     'token_type': 'Bearer'
-#                 }
-#             }
-#         }
-#     }
-# }
-#
+RESPONSE_LOGIN_EXAMPLE = {
+    200: {
+        'content': {
+            'application/json': {
+                'example': {
+                    'access_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJVc2VyQWRtaW4iLCJkYXRlIjoiMjAyNC0wMi0wNCAxNzozOToxMC43NjI1NzMiLCJleHAiOjE3MDcwNTc1NTB9.utFkx8oFaTEYfDStUmQV2lM7yK51IK87cjxVbqZCk0k',
+                    'expires_in': 600,
+                    'refresh_token': 'cc9583a2-85d7-504a-b518-b0d86506b749',
+                    'token_type': 'Bearer'
+                }
+            }
+        }
+    },
+    400: {
+        'content': {
+            'application/json': {
+                'examples': {
+                    'Введен юзернейм и почта': {
+                        'value': {
+                            'detail': 'Введите username или email.'
+                        }
+                    },
+                    'Отсутствует username': {
+                        'value': {
+                            'detail': 'Отсутствует username или email.'
+                        }
+                    },
+                    'Отсутствует email': {
+                        'value': {
+                            'detail': 'Отсутствует username или email'
+                        }
+                    },
+                    'Неверно введены данные': {
+                        'value': {
+                            'detail': 'Пароль или логин введен неверно.'
+                        }
+                    }
+                }
+            }
+        }
+    },
+    422: {
+        'content': {
+            'application/json': {
+                'examples': {
+                    'Невалидный E-mail (отсутствие "@")': {
+                        'value': {
+                            'detail': [
+                                {
+                                    'msg': 'value is not a valid email address: '
+                                           'The email address is not valid. '
+                                           'It must have exactly one @-sign.'
+                                }
+                            ]
+                        }
+                    },
+                    'Невалидный E-mail (отсутствие точки в конце)': {
+                        'value': {
+                            'detail': [
+                                {
+                                    'msg': 'value is not a valid email address: '
+                                           'The part after the @-sign is not valid. '
+                                           'It should have a period.'
+                                }
+                            ]
+                        }
+                    },
+                    'Невалидный E-mail': {
+                        'value': {
+                            'detail': [
+                                {
+                                    'msg': 'value is not a valid email address: '
+                                           'There must be something before the @-sign.'
+                                }
+                            ]
+                        }
+                    },
+                    'Длина почты меньше 6 символов': {
+                        'value': {
+                            'detail': [
+                                {
+                                    'msg': 'Value should have at least 6 items after validation, not 5'
+                                }
+                            ]
+                        }
+                    },
+                    'Длина почты больше 191 символ': {
+                        'value': {
+                            'detail': [
+                                {
+                                    'msg': 'value is not a valid email address: '
+                                           'Length must not exceed 2048 characters'
+                                }
+                            ]
+                        }
+                    },
+                }
+            }
+        }
+    }
+}
+
 # RESPONSE_USER_GET_EXAMPLE = {
 #     200: {
 #         'content': {'application/json':
