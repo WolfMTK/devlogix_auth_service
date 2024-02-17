8cc0050d617c14384c32a2c027a91b8ae1c26a25
AngryWolf <qrer71@yandex.ru>
2024-02-17 17:23:14 +0300

    Update config

--- src/auth/core/config.py
+++ src/auth/core/config.py
@@ -10,5 +10,6 @@ class Settings:
     )
     secret_token: str = os.getenv('SECRET_TOKEN', 'token')
     algorithm: str = os.getenv('ALGORITHM', 'HS256')
+    redis: str = os.getenv('REDIS_URL', 'redis://localhost')
     time_access_token: int = os.getenv('TIME_ACCESS_TOKEN', 24 * 60 * 60)
     time_refresh_token: int = os.getenv('TIME_REFRESH_TOKEN', 24 * 60 * 60)
