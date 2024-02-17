b1b4068b6a085ae4234fb87a4d44ebb867c10259
AngryWolf <qrer71@yandex.ru>
2024-02-17 14:35:15 +0300

    Create to get a user

--- src/auth/application/repositories/users.py
+++ src/auth/application/repositories/users.py
@@ -1,38 +1,48 @@
 from typing import Sequence
 
-from sqlalchemy import select, and_
+from sqlalchemy import select, and_, or_
 
 from auth.application.protocols.repository import SQLAlchemyRepository
-from auth.domain.models.users import User
+from auth.domain.models import User
 
 
 class UserRepository(SQLAlchemyRepository):
     model = User
 
+    async def get_user(self, username: str, email: str) -> User | None:
+        stmt = (select(self.model)
+                .where(or_(self.model.username == username,
+                           self.model.email == email)))
+        result = await self.session.execute(stmt)
+        return result.scalar_one_or_none()
+
     async def get_user_exists(self, **filter_by: dict[str, str]) -> bool:
-        stmt = select(self.model).filter_by(**filter_by).exists()
+        stmt = (select(self.model)
+                .where(self.model.is_active == True)
+                .filter_by(**filter_by).exists())
         return await self.session.scalar(select(stmt))
 
     async def check_username_user(self, id: int, username: str) -> bool:
-        stmt = select(self.model).filter(and_(
-            self.model.id != id,
-            self.model.username == username
-        )).exists()
+        stmt = (select(self.model)
+                .filter(and_(self.model.id != id,
+                             self.model.username == username))
+                .exists())
         return await self.session.scalar(select(stmt))
 
     async def check_email_user(self, id: int, email: str) -> bool:
-        stmt = select(self.model).filter(and_(
-            self.model.id != id,
-            self.model.email == email
-        )).exists()
+        stmt = (select(self.model)
+                .filter(and_(self.model.id != id,
+                             self.model.email == email))
+                .exists())
         return await self.session.scalar(select(stmt))
 
     async def get_users(self,
                         skip: int,
                         limit: int,
                         **filter_by: dict[str, str]) -> Sequence[User]:
-        stmt = select(self.model).filter_by(
-            **filter_by
-        ).limit(limit).offset(skip)
+        stmt = (select(self.model)
+                .filter_by(**filter_by)
+                .limit(limit)
+                .offset(skip))
         result = await self.session.execute(stmt)
         return result.scalars().all()
