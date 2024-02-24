c693ef17c189290f4ea2a7de986cec674bcd8990
AngryWolf <qrer71@yandex.ru>
2024-02-24 14:12:01 +0300

    Update imports

--- src/auth/api/dependencies.py
+++ src/auth/api/dependencies.py
@@ -1,19 +1,8 @@
 from typing import Annotated
 
-from redis.asyncio.client import Pipeline
 from fastapi import Depends
+from redis.asyncio.client import Pipeline
 
-from auth.application.protocols.unit_of_work import (
-    UnitOfWork,
-    UoW,
-)
-from auth.infrastructure.db import async_session_maker
 from auth.infrastructure.redis import create_connect
 
-
-def connect_database() -> UnitOfWork:
-    return UnitOfWork(async_session_maker)
-
-
-UoWDep = Annotated[UoW, Depends(connect_database)]
 RedisConnect = Annotated[Pipeline, Depends(create_connect)]
