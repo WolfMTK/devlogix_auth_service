f56291412ac01dff3eae903d0fcaaab619bc732b
AngryWolf <qrer71@yandex.ru>
2024-03-02 17:23:42 +0300

    Update imports

---
+++ src/auth/application/models/__init__.py
@@ -0,0 +1,6 @@
+from .tokens import TokenData, TokenGet, TokenUpdate
+from .users import UserGet, UserCreate, UserUpdate, UserLogin
+
+__all__ = ('TokenData', 'TokenGet', 'TokenUpdate',
+           'UserGet', 'UserCreate', 'UserUpdate',
+           'UserLogin')
--- src/auth/domain/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Base
-from .tokens import Token
-from .users import User
-
-__all__ = ('Base', 'Token', 'User')
