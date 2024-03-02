5e5695a68cffad84de977e5e35652a273dd3d073
AngryWolf <qrer71@yandex.ru>
2024-03-02 16:38:40 +0300

    Update di and imports

--- src/auth/main/di.py
+++ src/auth/main/di.py
@@ -1,13 +1,30 @@
 from functools import partial
 
+import redis.asyncio as aioredis
 from fastapi import FastAPI
+from redis.client import AbstractRedis
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from auth.application.protocols.unit_of_work import UoW, UnitOfWork
-from auth.infrastructure.db import create_async_session_maker
+from auth.adapters.sqlalchemy_db.database import Session
+from auth.application.protocols.database import UoWDatabase
+from auth.core import Settings
+
+
+def create_redis_connect():
+    return aioredis.from_url(Settings.redis).pipeline()
+
+
+def create_async_session_maker():
+    engine = create_async_engine(
+        url=Settings.db_url,
+        echo=False
+    )
+    return async_sessionmaker(engine, expire_on_commit=False)
 
 
 def init_dependencies(app: FastAPI) -> None:
-    app.dependency_overrides[UoW] = partial(
-        UnitOfWork,
+    app.dependency_overrides[UoWDatabase] = partial(
+        Session,
         create_async_session_maker()
     )
+    app.dependency_overrides[AbstractRedis] = create_redis_connect
