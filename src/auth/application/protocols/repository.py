6e7dfa3531e268b38c1b06988d932e028addaf4e
AngryWolf <qrer71@yandex.ru>
2024-03-02 16:38:20 +0300

    Update protocols

--- src/auth/application/protocols/repository.py
+++ src/auth/application/protocols/repository.py
@@ -1,10 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import TypeVar, Sequence, Any
+from typing import TypeVar
 
-from sqlalchemy import insert, select, delete, update
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from auth.infrastructure.db import Base
+from auth.domain.models import Base
 
 ModelType = TypeVar('ModelType', bound=Base)
 
@@ -29,46 +26,3 @@ class AbstractRepository(ABC):
     @abstractmethod
     async def delete_one(self, *args, **kwargs):
         ...
-
-
-class SQLAlchemyRepository(AbstractRepository):
-    model: ModelType | None = None
-
-    def __init__(self, session: AsyncSession) -> None:
-        self.session = session
-
-    async def add_one(
-            self,
-            **kwargs: dict[str, Any]
-    ) -> ModelType:
-        """Добавление записи в БД."""
-        stmt = insert(self.model).values(**kwargs).returning(self.model)
-        result = await self.session.execute(stmt)
-        return result.scalar_one()
-
-    async def update_one(self, id: int, **kwargs) -> ModelType:
-        """Обновление записи в БД."""
-        stmt = update(self.model).filter(
-            self.model.id == id
-        ).values(**kwargs).returning(self.model)
-        result = await self.session.execute(stmt)
-        return result.scalar_one()
-
-    async def find_one(self, **filter_by: dict[str, Any]) -> ModelType | None:
-        """Поиск записи в БД."""
-        stmt = select(self.model).filter_by(**filter_by)
-        result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
-
-    async def find_all(
-            self, order_by: str | None = None
-    ) -> Sequence[ModelType]:
-        """Поиск записей в БД."""
-        stmt = select(self.model).order_by(order_by)
-        result = await self.session.execute(stmt)
-        return result.scalars().all()
-
-    async def delete_one(self, **filter_by: dict[str, Any]) -> None:
-        """Удаление записи в БД."""
-        stmt = delete(self.model).filter_by(**filter_by)
-        await self.session.execute(stmt)
