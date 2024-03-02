7855e71a36da637097f052978487ed9d603c298f
AngryWolf <qrer71@yandex.ru>
2024-03-02 16:38:27 +0300

    Delete file

--- src/auth/api/dependencies.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from redis.asyncio.client import Pipeline
-
-from auth.infrastructure.redis import create_connect
-
-RedisConnect = Annotated[Pipeline, Depends(create_connect)]
