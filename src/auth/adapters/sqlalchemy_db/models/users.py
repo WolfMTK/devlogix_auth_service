90f0f0dd62aec0bd0feb306e91fdb2848d09d451
AngryWolf <qrer71@yandex.ru>
2024-03-03 14:57:09 +0300

    Create a model with blocked users

--- src/auth/adapters/sqlalchemy_db/models/users.py
+++ src/auth/adapters/sqlalchemy_db/models/users.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import uuid
 
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.expression import true
 
@@ -39,6 +39,10 @@ class Users(Base):
         back_populates='user',
         lazy='joined'
     )
+    banned_users: Mapped['BannedUsers'] = relationship(
+        back_populates='user',
+        lazy='selectin'
+    )
     created_at: Mapped[dt.datetime] = mapped_column(default=dt.datetime.now())
     is_active: Mapped[bool] = mapped_column(
         nullable=False,
@@ -55,3 +59,18 @@ class Users(Base):
             if hasattr(schema, key):
                 setattr(schema, key, value)
         return schema
+
+
+class BannedUsers(Base):
+    """Модель заблокированных пользователей."""
+    __tablename__ = 'banned_users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    date_blocked: Mapped[dt.datetime] = mapped_column(
+        default=dt.datetime.now()
+    )
+    end_date_block: Mapped[dt.datetime]
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['Users'] = relationship(
+        back_populates='banned_users',
+        lazy='selectin'
+    )
