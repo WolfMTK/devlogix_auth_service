293ccfd6f76b63f35f3dd6c13ee3ea2b0e43fa48
AngryWolf <qrer71@yandex.ru>
2024-03-03 15:08:33 +0300

    Add datetime format

--- src/auth/core/constants.py
+++ src/auth/core/constants.py
@@ -2,3 +2,4 @@ LENGTH_PASSWORD = 8
 LENGTH_USERNAME = 6
 PATTERN_PASSWORD = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!#$%&()*+,-./:;<=>?@[\]^_`{|}~]).{8,}$'
 TIMEZONE = 'Europe/Moscow'
+DATETIME_BANNED_FORMAT = '%Y-%m-%d'
