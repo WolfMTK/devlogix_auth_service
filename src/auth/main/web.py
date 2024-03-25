bb75da20b0595345b5e0978c9a7b9d3105da190b
AngryWolf <qrer71@yandex.ru>
2024-03-25 23:41:28 +0300

    Add middleware

--- src/auth/main/web.py
+++ src/auth/main/web.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth.core.base import Base  # noqa
 from auth.main.di import init_dependencies
@@ -12,8 +13,19 @@ from auth.user.presentation.interactor_factory import UserInteractorFactory
 from auth.user.presentation.web import user, auth
 
 
+def set_middleware(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['GET', 'POST', 'PATCH', 'DELETE'],
+        allow_headers=['*']
+    )
+
+
 def create_app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
+    set_middleware(app)
     init_dependencies(app)
     app.dependency_overrides.update(
         {StubUserGateway: new_user_gateway,
