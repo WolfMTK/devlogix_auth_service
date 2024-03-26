a589e013eaeb336cf1b729182a50f4dd6f555fcf
AngryWolf <qrer71@yandex.ru>
2024-03-26 20:55:31 +0300

    Update di

--- src/auth/main/di.py
+++ src/auth/main/di.py
@@ -2,7 +2,9 @@ from collections.abc import AsyncIterator
 from functools import partial
 from typing import TypeVar
 
+import redis.asyncio as aioredis
 from fastapi import Depends, FastAPI
+from redis.asyncio.client import AbstractRedis
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
@@ -15,7 +17,17 @@ from auth.common.application.protocols.jwt import TokenProvider
 from auth.common.application.protocols.password import PasswordProvider
 from auth.common.application.protocols.uow import UoW
 from auth.common.presentation.dependencies.depends_stub import Stub
-from auth.core.config import load_database_config, load_jwt_config
+from auth.core.config import (
+    load_database_config,
+    load_jwt_config,
+    load_redis_config,
+)
+from auth.token.adapters.stub_db import StubTokenGateway
+from auth.token.application.protocols.redis import RedisUoW
+from auth.token.presentation.dependencies.gateway import new_token_gateway
+from auth.token.presentation.dependencies.redis import new_redis_connect
+from auth.user.adapters.stub_db import StubUserGateway
+from auth.user.presentation.dependencies.gateway import new_user_gateway
 
 DependencyT = TypeVar("DependencyT")
 
@@ -45,11 +57,16 @@ def init_dependencies(app: FastAPI):
         secret_token=jwt_config.secret_token,
         algorithm=jwt_config.algorithm
     )
+    redis_config = load_redis_config()
     app.dependency_overrides.update(
         {
             AsyncSession: partial(create_async_session, session_maker),
             UoW: new_uow,
             PasswordProvider: PasswordCryptoProvider,
             TokenProvider: lambda: jwt,
+            RedisUoW: new_redis_connect,
+            StubUserGateway: new_user_gateway,
+            StubTokenGateway: new_token_gateway,
+            AbstractRedis: lambda: aioredis.from_url(redis_config.redis_uri)
         }
     )
