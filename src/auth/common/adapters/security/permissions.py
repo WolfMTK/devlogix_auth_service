2991b4f41b6611f49dde451cd187c54f4e61fd40
AngryWolf <qrer71@yandex.ru>
2024-03-26 21:21:16 +0300

    Update imports to add method checking token in redis

--- src/auth/common/adapters/security/permissions.py
+++ src/auth/common/adapters/security/permissions.py
@@ -7,6 +7,9 @@ from jwt import PyJWTError
 from auth.common.application.protocols.jwt import TokenProvider
 from auth.common.application.protocols.permissions import BearerProvider
 from auth.common.presentation.dependencies.depends_stub import Stub
+from auth.token.application.protocols.redis import RedisUoW
+from auth.user.adapters.database.models import User
+from auth.user.adapters.stub_db import StubUserGateway
 
 bearer_token = HTTPBearer(auto_error=False)
 
@@ -16,17 +19,27 @@ class PermissionBearerProvider(BearerProvider):
             self,
             bearer: HTTPAuthorizationCredentials = Depends(bearer_token),
             jwt: TokenProvider = Depends(Stub(TokenProvider)),
+            redis: RedisUoW = Depends(),
+            gateway: StubUserGateway = Depends()
     ) -> None:
         self.bearer = bearer
         self.jwt = jwt
+        self.redis = redis
+        self.gateway = gateway
 
-    async def get_username(self) -> str:
+    async def get_username(self) -> User:
         try:
             token = await self._get_credentials()
             if (username := self.jwt.decode_token(token).get('sub')) is None:
                 return self._is_unauthorized()
-            return username
+            user = await self.gateway.get_user(username=username)
+            await self._check_token(f'access-token::{user.id}')
+            return user
         except PyJWTError:
+            return self._is_unauthorized()
+
+    async def _check_token(self, token: str) -> None:
+        if await self.redis.get(token) is None:
             return self._is_unauthorized()
 
     def _is_unauthorized(self) -> Never:
