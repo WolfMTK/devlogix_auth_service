bc0eac0cc53c19e1bd9376de9bae0d0db4ee7abe
AngryWolf <qrer71@yandex.ru>
2024-03-26 21:20:36 +0300

    Update imports and to get profile

--- src/auth/user/application/get_me.py
+++ src/auth/user/application/get_me.py
@@ -1,25 +1,10 @@
 from auth.common.application.protocols.interactor import Interactor
-from auth.common.application.protocols.uow import UoW
-from auth.user.adapters.stub_db import StubUserGateway
-from auth.user.domain.models.user import UserMeDTO, UserResultDTO
-from auth.user.domain.services.user import UserService
+from auth.user.adapters.database.models import User
+from auth.user.domain.models.user import UserResultDTO
 
 
-class GetUserMe(Interactor[UserMeDTO, UserResultDTO]):
-    def __init__(
-            self,
-            uow: UoW,
-            user_db_gateway: StubUserGateway,
-            user_service: UserService
-    ) -> None:
-        self.uow = uow
-        self.user_db_gateway = user_db_gateway
-        self.user_service = user_service
-
-    async def __call__(self, user: UserMeDTO) -> UserResultDTO:
-        model = await self.user_db_gateway.get_user(username=user.username)
-        user = self.user_service.get_user_me(model)
-        self.user_service.check_is_active(user)
+class GetUserMe(Interactor[User, UserResultDTO]):
+    async def __call__(self, user: User) -> UserResultDTO:
         return UserResultDTO(
             id=user.id,
             username=user.username,
