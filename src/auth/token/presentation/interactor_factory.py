e2d33266552036fc6ce9373cd189b0f5cdc9f9a0
AngryWolf <qrer71@yandex.ru>
2024-03-26 23:07:57 +0300

    Create method to delete token

--- src/auth/token/presentation/interactor_factory.py
+++ src/auth/token/presentation/interactor_factory.py
@@ -2,9 +2,14 @@ from abc import ABC, abstractmethod
 from contextlib import AbstractAsyncContextManager
 
 from auth.token.application.create_token import CreateToken
+from auth.token.application.delete_token import DeleteToken
 
 
 class TokenInteractorFactory(ABC):
     @abstractmethod
     def create_token(self) -> AbstractAsyncContextManager[CreateToken]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_token(self) -> AbstractAsyncContextManager[DeleteToken]:
         raise NotImplementedError
