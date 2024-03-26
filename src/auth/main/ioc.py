4ccbfa7fa612b61a8925bdd733ea62cb9b7160eb
AngryWolf <qrer71@yandex.ru>
2024-03-26 20:58:49 +0300

    Update imports

--- src/auth/main/ioc.py
+++ src/auth/main/ioc.py
@@ -1,5 +1,5 @@
 from collections.abc import AsyncIterator
-from contextlib import asynccontextmanager, AbstractAsyncContextManager
+from contextlib import asynccontextmanager
 
 from fastapi import Depends
 
