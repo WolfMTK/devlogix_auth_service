90577a83877389f77075cfe875db669b7b9faef8
AngryWolf <qrer71@yandex.ru>
2024-03-26 22:50:25 +0300

    Remove the check method for the active user

--- src/auth/user/domain/services/user.py
+++ src/auth/user/domain/services/user.py
@@ -19,11 +19,5 @@ class UserService:
                 'A user with this username already exists.'
             )
 
-    def check_is_active(self, user: User) -> None:
-        if not user.is_active:
-            raise InactiveUserException(
-                'Invalid authentication credentials.'
-            )
-
     def set_is_active_user(self, user: User) -> None:
         user.is_active = True
