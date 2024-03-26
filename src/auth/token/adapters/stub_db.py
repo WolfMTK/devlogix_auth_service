012bedbe65f39a35e78cd3edaa52b172e8d064c4
AngryWolf <qrer71@yandex.ru>
2024-03-26 23:08:56 +0300

    Update imports

--- src/auth/token/adapters/stub_db.py
+++ src/auth/token/adapters/stub_db.py
@@ -1,6 +1,9 @@
-from auth.token.application.protocols.gateway import TokenCreated
+from auth.token.application.protocols.gateway import (
+    TokenCreated,
+    TokenDeleted,
+)
 from auth.user.application.protocols.gateway import UserCheck, UserReading
 
 
-class StubTokenGateway(TokenCreated, UserCheck, UserReading):
+class StubTokenGateway(TokenCreated, UserCheck, UserReading, TokenDeleted):
     pass
