a0da8ed2f13d87cb541e3f88f8c3e3b92acb9823
AngryWolf <qrer71@yandex.ru>
2024-03-26 20:56:46 +0300

    Update imports

--- src/auth/main/web.py
+++ src/auth/main/web.py
@@ -4,11 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from auth.core.base import Base  # noqa
 from auth.main.di import init_dependencies
 from auth.main.ioc import UserIOC, TokeIOC
-from auth.token.adapters.stub_db import StubTokenGateway
-from auth.token.presentation.dependencies.gateway import new_token_gateway
 from auth.token.presentation.interactor_factory import TokenInteractorFactory
-from auth.user.adapters.stub_db import StubUserGateway
-from auth.user.presentation.dependencies.gateway import new_user_gateway
 from auth.user.presentation.interactor_factory import UserInteractorFactory
 from auth.user.presentation.web import user, auth
 
@@ -28,9 +24,7 @@ def create_app() -> FastAPI:
     set_middleware(app)
     init_dependencies(app)
     app.dependency_overrides.update(
-        {StubUserGateway: new_user_gateway,
-         StubTokenGateway: new_token_gateway,
-         UserInteractorFactory: UserIOC,
+        {UserInteractorFactory: UserIOC,
          TokenInteractorFactory: TokeIOC}
     )
     app.include_router(user.router)
