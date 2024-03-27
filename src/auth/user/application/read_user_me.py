11e65005cef4aa12e805e8444807a1c57f101b22
AngryWolf <qrer71@yandex.ru>
2024-03-27 21:39:46 +0300

    Update imports and rename module

--- src/auth/user/application/read_user_me.py
+++ src/auth/user/application/read_user_me.py
@@ -1,11 +1,11 @@
 from auth.common.application.protocols.interactor import Interactor
 from auth.user.adapters.database.models import User
-from auth.user.domain.models.user import UserResultDTO
+from auth.user.domain.models.user import UserResult
 
 
-class GetUserMe(Interactor[User, UserResultDTO]):
-    async def __call__(self, user: User) -> UserResultDTO:
-        return UserResultDTO(
+class GetUserMe(Interactor[User, UserResult]):
+    async def __call__(self, user: User) -> UserResult:
+        return UserResult(
             id=user.id,
             username=user.username,
             email=user.email
